5b75a0f66bdc06ef7804b0e5c36411561aa04a82
jboe26 <joshrboepple@outlook.com>
2019-08-11 20:59:31 -0400

    uipdated JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,36 @@
 $(document).ready(function () {
     console.log("ready!");
 
-    setTimeout(function, 120000 )
+    //Declare variable to hold setInterval timer
+    var intervalID;
 
+    //Declare variable equal to the start time
+    var time = 15;
+
+    //declare variable to track the number of questions answered correctly
+    var correct = 0;
+
+    //create a function to hide the start button when clicked
+    $("#startButton").click(function () {
+        $(".start").hide();
+        $(".questions").show();
+
+        // create timer to countdown from 15 seconds once the start button is clicked
+        intervalID = setInterval(count, 1000);
+        function count() {
+            $("#timeRemaining-text").text("You have " + time + " seconds left.");
+            time--;
+
+            if (time < 0) {
+                $(".questions").hide();
+                clearInterval(intervalID);
+                // $("#timeRemaining-text").hide();
+                // $("#done-text").show();
+                // $("#done-text").text("You answered " + correct + " out of the 6 questions correctly.");
+
+            }
+        }
+    });
 
 
 
