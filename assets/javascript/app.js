1cd9158c382726796049da5a40128d6eb768cbdf
jboe26 <joshrboepple@outlook.com>
2019-08-14 17:18:27 -0400

    updated JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,43 +15,56 @@ $(document).ready(function () {
         $(".timer").text("You have " + time + " seconds remaining.");
         time--;
 
-    if (time < 0) {
-        $(".questions").hide();
-        clearInterval(intervalID);
-    }
+        if (time < 0) {
+            $(".questions").hide();
+            clearInterval(intervalID);
+        }
 
 
 
-    //declare variable to track the number of questions answered correctly
-    var correct = 0;
+        //declare variable to track the number of questions answered correctly
+        var correct = 0;
 
-    //create a function to hide the start button when clicked
-    $("#startButton").click(function () {
-        $("#startButton").hide();
+        //create a function to hide the start button when clicked
+        $("#startButton").click(function () {
+            $("#startButton").hide();
 
-        $(".questions").hide();
-        $(".questions").show();
-
-
-    
-    });
-
-    $("#doneButton").click(function () {
-        $(".questions").hide();
-        clearInterval(intervalID);
-
-    var answers = ["a","b","c","d","e","f"];
-    
-    var correctAnswers = 0;
-
-    // for (kliojp         )
-
-    $("#correctAnswers").text("You got " + correctAnswers +" questions right.")
+            $(".questions").hide();
+            $(".questions").show();
 
 
-    
-});
 
-    
+        });
+
+        $("#doneButton").click(function () {
+            $(".questions").hide();
+            clearInterval(intervalID);
+
+            var answers = ["#a", "#b", "#c", "#d", "#e", "#f"];
+
+            console.log(answers);
+
+            // trying to use this variable, not sure where-- i want it to be the correct answers for the questions. 
+            
+            // answers 
+
+            var correctAnswers = 0;
+            var missed = 0;
+            var incorrectAnswers = 0;
+
+           if ( (answers = "#a") && (answers = "#b") && (answers = "#c") && (answers = "#d") && (answers = "#e") && (answers = "#f") ) {
+               
+           }
+                
+
+                
+
+            $("#correctAnswers").text("You got " + correctAnswers + " questions right.");
+            $("#missed").text("You missed " + missed + " questions.");
+            $("#incorrectAnswers").text("You missed " + incorrectAnswers + " questions.")
+
+        });
+
+
     }
 });
